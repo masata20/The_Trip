64f0cd8c80bbe4085a3894deb8cc1f9d2a872efb
Masataka Mori <masataka.mori20@gmail.com>
2017-07-12 17:59:47 +0900

    fixed issue when the trancated value after 2 decimals add up to more than 1

--- main.cpp
+++ main.cpp
@@ -27,24 +27,35 @@ int main()
 		// don't forget change size to float
 		float average_cost = sum / (float)size;
 		float exchange_cost = 0.0;
+
+		float tranc_decimals = 0.0;
 		// calculate the diff of each with average, need to cut off after 2 decimals
 		// add up the only positive diffences
 		// or do only half of them iff it's sorted(maybe slower than above)
 		for (int i = 0; i < size; i++)
 		{
 			float diff = average_cost - costs[i];
-			
 			if(diff > 0.0)
 			{
 				// trancate after 2 decimals
 				float temp = diff * 100;
 				int diff_int = temp;
+				tranc_decimals += temp - diff_int;
 				float trancated_diff = float(diff_int)/float(100);
-				
+
+
 				exchange_cost += trancated_diff;
 			}
 
 
+		}
+
+		
+		if (tranc_decimals >= 1)
+		{
+			int diff_int = tranc_decimals;
+			exchange_cost += float(diff_int)/float(100);
+		
 		}
 
 		printf("%.2f\n", exchange_cost);
